442913f2720d2815ce98a5d5803ed8abe8dec1a2
unknown <savellsa@ashleyautomation.com>
2019-10-28 12:36:01 -0500

    role to role_id in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('discord_id');
-            $table->integer('user_role');
+            $table->integer('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
